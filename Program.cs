982004b2534cc1e3a4e9c5d7ac198358cc2d9f5e
shreesharao <sshreesha4@gmail.com>
2017-11-10 18:12:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleToWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StartWebHost();
+            BuildWebHostUsingWebHostBuilder().Run();
+            //BuildWebHostUsingDefaultBuilder().Run();
+            //BuildWebHostUsingConfigurationBuilder(args).Run();
+
+        }
+
+        private static void StartWebHost()
+        {
+            using (var host = WebHost.Start(app => app.Response.WriteAsync("hello")))
+            {
+                Console.WriteLine("started");
+                host.WaitForShutdown();
+            }
+        }
+
+        private static IWebHost BuildWebHostUsingWebHostBuilder()
+        {
+            var webHostBuilder = new WebHostBuilder();
+
+            return webHostBuilder.
+                                CaptureStartupErrors(true).
+                                UseContentRoot(Directory.GetCurrentDirectory()).
+                                 UseUrls("http://localhost:3510").  //can not have https
+                                 //UseHttpSys().
+                                UseKestrel(options:options=> {
+                                    
+                                }).   //overrides previous setting
+                                UseIISIntegration().
+                                UseStartup<StartUp>().
+                                UseSetting(WebHostDefaults.PreventHostingStartupKey, "true").  //could not see it working
+                               
+                                Build();
+
+        }
+
+        private static IWebHost BuildWebHostUsingDefaultBuilder()
+        {
+            return WebHost.CreateDefaultBuilder().
+                UseStartup<StartUp>().
+                Build();
+        }
+
+        private static IWebHost BuildWebHostUsingConfigurationBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().
+                                                        SetBasePath(Directory.GetCurrentDirectory()).
+                                                        AddJsonFile("appsettings.json", optional: true).
+                                                        AddCommandLine(args).  //use dotnet watch run --urls http://localhost:3000 to overrride urls
+                                                        Build();
+
+            return WebHost.CreateDefaultBuilder().
+                                                 UseStartup<StartUp>().
+                                                 UseConfiguration(configuration).
+                                                 Build();
+
+        }
+
+
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+
+namespace ConsoleToWeb
+{
+    public class StartUp
+    {
+        public StartUp()
+        {
+            Configuration = new ConfigurationBuilder().
+                SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("appsettings.json").Build();
+        }
+
+        private IConfiguration Configuration;
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvc();
+
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment,IApplicationLifetime applicationLifetime)
+        {
+            applicationLifetime.ApplicationStarted.Register(OnStarted);
+            applicationLifetime.ApplicationStopping.Register(OnStopping);
+            applicationLifetime.ApplicationStopped.Register(OnStopped);
+
+
+            if (hostingEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseBrowserLink();
+            }
+
+            applicationBuilder.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{Controller=Home}/{Action=Index}/{id?}");
+            });
+
+            applicationBuilder.UseStaticFiles();
+        }
+
+        private void OnStopping()=>Console.WriteLine($"Application stopping");
+        
+        private void OnStarted() => Console.WriteLine($"Application started");
+        private void OnStopped() => Console.WriteLine($"Application stopped");
+    }
+}
